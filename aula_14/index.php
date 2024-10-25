09b59a91a863a73def185b0cdb6784cab917b1e9
Julia Fitzlaff <julia.fitzlaff@unidavi.edu.br>
2024-10-25 20:14:35 -0300

    atualização

---
+++ aula_14/index.php
@@ -0,0 +1,41 @@
+<?php
+/*
+    -------Calculadora-------------------------------------------------
+    require_once "calculadora.php";
+
+    $calculadora = new Calculadora();
+    $calculadora->setOperador1(10);
+    $calculadora->setOperador2(10);
+    $calculadora->somar();
+
+    echo $calculadora->somar();
+
+    -------Computador--------------------------------------------------
+    require_once "computador.php";
+
+    $computador = new Computador();
+    $computador->ligar();
+    $computador->desligar();
+    
+    echo $computador->getStatus();
+
+    -------Jogador-----------------------------------------------------
+    require_once "time.php";
+    require_once "jogador.php";
+    
+    $time = new Time();
+
+    $time->setNome("Unidavi FC");
+    $time->setAnoFundacao(2000);
+
+    $jogador = new Jogador();
+    $jogador->setNome("Pelé");
+    $jogador->setPosicao("M");
+    $jogador->setDataNascimento(new DateTime("2000-01-01"));
+
+    $time->addJogador($jogador);
+
+    echo $time->getNome() . "<br>";
+    var_dump($time->getJogadores());
+*/ 
+?>
---
+++ aula_14/jogador.php
@@ -0,0 +1,20 @@
+<?php
+
+    class Jogador {
+        private $nome;
+        private $posicao;
+        private $dataNascimento;
+
+        public function setNome($nome) {
+            $this->nome = $nome;
+        }
+
+        public function setPosicao($posicao) {
+            $this->posicao = $posicao;
+        }
+
+        public function setDataNascimento($data) {
+            $this->dataNascimento = $data;
+        }
+    }
+?>
