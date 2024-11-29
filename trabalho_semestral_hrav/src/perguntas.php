bc1bc457774463c0863043889943a7fa1d03547b
Julia Fitzlaff <julia.fitzlaff@unidavi.edu.br>
2024-11-29 18:18:30 -0300

    atualização

--- trabalho_semestral_hrav/src/perguntas.php
+++ trabalho_semestral_hrav/src/perguntas.php
@@ -15,8 +15,8 @@ if (pg_num_rows($result) > 0) {
             <h2 class="h4 mx-3"><?php echo htmlspecialchars($descricao_pergunta); ?></h2>
             <div class="btn-group classificacao" role="group" aria-label="Opções para avaliação">
             <?php for ($i = 0; $i <= 10; $i++) { ?>
-            <input type="radio" class="btn-check" name="pergunta_<?php echo $id_pergunta; ?>" id="btnradio<?php echo $i; ?>_<?php echo $id_pergunta; ?>" value="<?php echo $i; ?>" autocomplete="off">
-            <label class="btn bg-color-<?php echo $i; ?>" for="btnradio<?php echo $i; ?>_<?php echo $id_pergunta; ?>"><?php echo $i; ?></label>
+                <input type="radio" class="btn-check" name="pergunta_<?php echo $id_pergunta; ?>" id="btnradio<?php echo $i; ?>_<?php echo $id_pergunta; ?>" value="<?php echo $i; ?>" autocomplete="off">
+                <label class="btn bg-color-<?php echo $i; ?>" for="btnradio<?php echo $i; ?>_<?php echo $id_pergunta; ?>"><?php echo $i; ?></label>
             <?php } ?>
             </div>
             <br>
