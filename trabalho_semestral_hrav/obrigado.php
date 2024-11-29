240e7e7c85be7cf639723410d7957a9d43947687
Julia Fitzlaff <julia.fitzlaff@unidavi.edu.br>
2024-11-29 18:40:45 -0300

    atualização

--- trabalho_semestral_hrav/obrigado.php
+++ trabalho_semestral_hrav/obrigado.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="refresh" content="5;url=index.php" />
-    <title>Avalia o Hospital</title>
+    <title>Obrigado(a)</title>
     <link rel="icon" href="./assets/favicon-32x32.png" type="image/png" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="./css/style.css" />
